822ae90bfd8e1495ad6b6d4a4afbdab99ad3e8b2
hikmatyar22 <abiq5161@gmail.com>
2024-06-04 11:12:12 +0700

    membuat exeption try dan catch serta bebrapa coutnya di dalam int main

--- StandarExseption/StandarExseption.cpp
+++ StandarExseption/StandarExseption.cpp
@@ -7,5 +7,19 @@ using namespace std;
 
 int main()
 {
-	
+	cout << "Awal program" << endl; //penanda 1.... 
+	try {
+		array <int, 3> data = { 1, 2, 3 }; 
+		//pesan array integer 3 elemen
+		//cout << data.at(5) << endl;
+		//memanggil array pada elemen ke 5
+	}
+	catch (exception&e) { 
+		//penangkap menggunakan obyek exeption
+		cout << "e.what()" << endl; 
+		// akan dieksekusi karena array data hanya memiliki 3 elemen
+	}
+	cout << "Baris Program Yang Terakhir" << endl;
+	//penanda 2 : bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
+	return 0;
 }
