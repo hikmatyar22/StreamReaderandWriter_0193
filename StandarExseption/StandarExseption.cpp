9146e4745aff8d2e01ffe0f1c036a07ce3093501
hikmatyar22 <abiq5161@gmail.com>
2024-06-04 11:08:41 +0700

    membuat include exception untuk obyek exception yang akan digunakan dan membuat include array untuk obyek array yang akan digunakan

---
+++ StandarExseption/StandarExseption.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan digunakan
+using namespace std;
+
+int main()
+{
+	
+}
