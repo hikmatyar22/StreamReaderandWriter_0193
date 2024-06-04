86f9306229555242bfad6c6aee30afc03e535383
hikmatyar22 <abiq5161@gmail.com>
2024-06-04 10:17:56 +0700

    mendeklarasi variabel baris menggunaka  tipe data string dan membuat ofstream untuk membuka file dalam mode menulis serta menunjukannya ke sebuah nama file

--- PRAK12PEMDAS_Iofile/PRAK12PEMDAS_Iofile.cpp
+++ PRAK12PEMDAS_Iofile/PRAK12PEMDAS_Iofile.cpp
@@ -4,5 +4,15 @@
 using namespace std;
 
 int main() {
+	//untuk menyimpan data berbentuk string
+	string baris;
 
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjukan ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	
 }
